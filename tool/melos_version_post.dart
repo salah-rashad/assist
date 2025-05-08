e7d1045667d736d9c472e612ac220453a589d678
Salah Rashad <salah.r.ahmed@gmail.com>
2025-05-08 07:08:52 +0300

    fix: add check for release commit before tagging in melos_version_post.dart

--- tool/melos_version_post.dart
+++ tool/melos_version_post.dart
@@ -9,10 +9,22 @@ Future<void> main() async {
 
 /// Add commit tag to git commit e.g. "assist-v0.3.0"
 Future<void> addCommitTag(String newVersion) async {
-  final process = await Process.run('git', ['tag', 'assist-v$newVersion']);
+  // Get the latest commit message
+  final logResult = await Process.run('git', ['log', '-1', '--pretty=%s']);
+  final lastCommitMessage = logResult.stdout.toString().trim();
 
-  if (process.exitCode != 0) {
-    stderr.writeln('❌ Error adding commit tag: ${process.stderr}');
+  if (!lastCommitMessage.startsWith('chore(release)')) {
+    stdout.writeln('ℹ️ Skipping tag: Last commit is not a release commit.');
+    return;
+  }
+
+  // Add the tag
+  final tagResult = await Process.run('git', ['tag', 'assist-v$newVersion']);
+
+  if (tagResult.exitCode != 0) {
+    stderr.writeln('❌ Error adding commit tag: ${tagResult.stderr}');
     exit(1);
+  } else {
+    stdout.writeln('✅ Tag assist-v$newVersion added successfully.');
   }
 }
