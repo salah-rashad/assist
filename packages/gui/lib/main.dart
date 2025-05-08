cbfd98fe637c65e2287dcbf03d0e2657cde80381
Salah Rashad <salah.r.ahmed@gmail.com>
2025-05-08 03:59:22 +0300

    feat(gui): update projectPath variable handling in main

--- packages/gui/lib/main.dart
+++ packages/gui/lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:assist_core/services/service.secure_storage.dart';
 import 'package:assist_gui/app/app.dart';
 import 'package:assist_gui/core/constants/constants.dart';
+import 'package:assist_gui/core/constants/env.dart';
 import 'package:assist_gui/core/utils/bloc_observer.dart';
 import 'package:bloc/bloc.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-Future<void> main() async {
+Future<void> main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   await SecureStorageManager.initialize();
@@ -14,12 +15,22 @@ Future<void> main() async {
   EasyLocalization.logger.enableLevels = [];
   Bloc.observer = AppBlocObserver();
 
+  // if project path is provided as an argument, use it
+  // otherwise, use the environment variable
+  //
+  // Project path is expected to be provided from arguments when running
+  // from release mode.
+  //
+  // And it is expected to be provided from environment variable when running
+  // from debug mode
+  final projectPath = args.isNotEmpty ? args[0] : Env.pwd;
+
   runApp(
     EasyLocalization(
       supportedLocales: Constants.supportedLocales,
       path: 'assets/translations',
       fallbackLocale: Locale('en'),
-      child: const MainApp(),
+      child: MainApp(projectPath: projectPath),
     ),
   );
 }
